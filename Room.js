fb2af7b71a8c24e4cc39b17c2c33d132501d5eca
csbanon <46613416+csbanon@users.noreply.github.com>
2019-11-12 16:58:46 -0500

    Update Room.js

--- Room.js
+++ Room.js
@@ -1,4 +1,5 @@
-class Room  {
+class Room
+{
     constructor(roomNum,reservation,isClean,isVacant) 
     {
         this.roomNum = roonNum;
@@ -7,40 +8,14 @@ class Room  {
         this.isVacant = isVacant;
     }
 
-    _isAvailable()
+    isAvailable()
     {
-        return isVacant;
+        return isVacant && isClean;
     }
 
-    cleanRoom() // ask about possible implementation
-    {
-        this.isClean = false;
-
-    }
-
-    _description()
-    {   
-        console.log(this.roomNum);
-        console.log("\n");
-        console.log(this.reservation);
-        console.log("\n");
-        if(this.isClean == true)
-        {
-            console.log("Room is clean\n");
-        }
-        else
-        {
-            console.log("Room is dirty\n");
-        }
-        if(this.isVacant == true)
-        {
-            console.log()
-        }
-        else
-        {
-            console.log("Room is occupied\n");
-        }
-    }
-
-
+//     cleanRoom()
+//     {
+//         this.isClean = false;
+//         // Set the service.
+//     }
 }
