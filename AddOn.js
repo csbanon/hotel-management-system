523b15dc328c153431e5cc98b5775d8092c2829f
fuanonemus <56282190+fuanonemus@users.noreply.github.com>
2019-11-18 23:49:58 -0500

    removed description and tostring

--- AddOn.js
+++ AddOn.js
@@ -6,20 +6,4 @@ class AddOn
         this.description = description;
         this.price = price;
     }
-
-    static description ()
-    {
-        // This class keeps track of details for AddOns include name, description, and price.
-    }
-
-    // returns in the format:
-    // [Name], $[price]: [description]
-    toString()
-    {
-        var s = this.name;
-        s = s.concat(", $");
-        s = s.concat(this.price.toFixed(2));
-        s = s.concat(": ");
-        return s.concat(this.description);
-    }
 }
