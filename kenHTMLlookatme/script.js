c10ebfd9dd8ad80a50423d86c7e7bf0bf6ea5ba8
kenken532 <46572409+kenken532@users.noreply.github.com>
2019-11-29 15:47:33 -0500

    Update script.js

--- kenHTMLlookatme/script.js
+++ kenHTMLlookatme/script.js
@@ -7,15 +7,12 @@
       function($http, $scope, $window) {
   
         $scope.master = {};
-
-
+        
         // Datepicker
         $scope.monthOptions = [01, 02, 03, 
           04, 05, 06, 07, 
           08, 09, 10, 
           11, 12];
-
-
 
         $scope.yearOptions = [2020,2021,2022,2023,2024,2025,2026,2027,2028,2029,2030];
 
@@ -49,10 +46,50 @@
           startingDay: 1
         };
 
-        $scope.submitForm = function(isValid, user) {
+        $scope.formats = ['dd-MMMM-yyyy', 'dd/MM/yyyy', 'dd.MM.yyyy', 'shortDate'];
+        $scope.format = $scope.formats[0];
 
-          $scope.master = angular.copy(user);
+        $scope.dateCheck = function(date) 
+        {
+          revisedDate = new Date(date);
+          return revisedDate.toDateString();
+        }
 
+        $scope.subTotalCalc = function(date1, date2) {
+          var startDate1 = new Date(date1);
+          var endDate1 = new Date(date2);
+          var timeEst = endDate1.getTime() - startDate1.getTime();
+          var daysEst = timeEst/(1000*3600*24);
+          var subtotal = daysEst * 50.00;
+          return subtotal.toFixed(2);
+        }
+
+        $scope.taxCalc = function(date1, date2) {
+          var startDate1 = new Date(date1);
+          var endDate1 = new Date(date2);
+          var timeEst = endDate1.getTime() - startDate1.getTime();
+          var daysEst = timeEst/(1000*3600*24);
+          var subtotal = daysEst * 50.00;
+          var tax = subtotal * 0.07;
+          return tax.toFixed(2);
+        }
+
+        $scope.finCalc = function(date1, date2) {
+          var startDate1 = new Date(date1);
+          var endDate1 = new Date(date2);
+          var timeEst = endDate1.getTime() - startDate1.getTime();
+          var daysEst = timeEst/(1000*3600*24);
+          var subtotal = daysEst * 50.00;
+          var tax = subtotal * 0.07;
+          var finTot = subtotal + tax;
+          return finTot.toFixed(2);
+        }
+
+
+
+
+
+        $scope.submitForm = function(isValid) {
             if(isValid) {
                 alert("Date 1: " + $scope.user.startDate + "\nDate 2: " + $scope.user.endDate);
             }
