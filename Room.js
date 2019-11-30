adc903e109183dbc1908f34b558c1856fbb6f634
ReidMatthew <46468779+ReidMatthew@users.noreply.github.com>
2019-11-29 21:06:54 -0500

    Added the read/store functions; needs pathing Stil

--- Room.js
+++ Room.js
@@ -1,21 +1,34 @@
-class Room
-{
-    constructor(roomNum,reservation,isClean,isVacant) 
-    {
-        this.roomNum = roonNum;
-        this.reservation = reservation;
-        this.isClean = isClean;
-        this.isVacant = isVacant;
-    }
+const fs = require('fs');
 
-    isAvailable()
-    {
-        return isVacant && isClean;
-    }
+class Room {
+  constructor(roomNum, reservation, isClean, isVacant) {
+    this.roomNum = roonNum;
+    this.reservation = reservation;
+    this.isClean = isClean;
+    this.isVacant = isVacant;
+  }
 
-//     cleanRoom()
-//     {
-//         this.isClean = false;
-//         // Set the service.
-//     }
+  isAvailable() {
+    return isVacant && isClean;
+  }
+
+  //     cleanRoom()
+  //     {
+  //         this.isClean = false;
+  //         // Set the service.
+  //     }
+
+  read() {
+    let rawdata = fs.readFileSync(this.path + this.roomNum + '.json');
+    let temp = JSON.parse(rawdata);
+    this.roomNum = temp.roonNum;
+    this.reservation = temp.reservation;
+    this.isClean = temp.isClean;
+    this.isVacant = temp.isVacant;
+  }
+
+  store() {
+    let data = JSON.stringify(this);
+    fs.writeFileSync(this.path + this.roomNum + ".json", data);
+  }
 }
